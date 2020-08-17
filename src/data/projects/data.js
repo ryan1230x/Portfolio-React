062b2eba6ebc6434ec027459b0f1a66db910b264
ryan1230x <ryan1230tenerife@gmail.com>
2020-08-17 17:19:21 +0100

    Project Information added

--- src/data/projects/data.js
+++ src/data/projects/data.js
@@ -17,3 +17,24 @@ export const helpdesk = {
   furtherDetails: [],
   link:'http://helpdesk.ryanwilliamharper.com'
 }
+
+export const makeup_by_talie = {
+  title:'Makeup By Talie',
+  introduction:'This is a project for a Makeup Artist, the client wanted a website to showcase her work and have a way to contact her to book appointments. This project was built using HTML, Sass, JavaScript, MySQL & PHP.',
+  furtherDetails:[
+    'This project was made from scratch, no framework was used, I wanted this to have a custom design. The layout through out the whole website was made with CSS grids.',
+    'There is a gallery section to showcase the clients work, I thought alot about loading time and performance while making and developing this project so I wanted to have as least dependencies as possible to increase the DOM load time as much as possible.',
+    'The gallery images have been stored in a MySQL database and these can be uploaded dynamically, for the database I normalized the structure, creating two tables one for large images and another for small images and using a MySQL join these are able to render on the screen.',
+    'I created a very simple Adminsitration portal so the client can upload images by herself, here I created a secure login system that can only be access when using the right credentails, I made sure that there was security in the database too by hashing the password using the most uptodate hashing algorithm.'
+  ],
+  link:'https://makeupbytalie.com'
+}
+
+export const eCommerce = {
+  title:'E-commerce',
+  introduction: 'A fictional e-commerce shop made Materialize on the Frontend and PHP & MySQL on the backend.',
+  furtherDetails: [],
+  link:'http://shop.ryanwilliamharper.com'
+}
+
+export const anilist = {}
