4e25c8de0408dec698a3e06673a8891ebf837746
ryan1230x <ryan1230tenerife@gmail.com>
2020-08-20 16:40:33 +0100

    finished Showcase Component

--- src/components/projectsPage/showCase.js
+++ src/components/projectsPage/showCase.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 class showCase extends React.Component {
-
   render () {
+    const { content, image } = this.props.data
     return (
       <Col xs={12} md={6}>
-        <p></p>
+        <p>{ content }</p>
       </Col>
-      <Col>
-        <img src="" />
+      <Col xs={12} md={6}>
+        <img
+          src={image}
+          className="img-fluid"
+        />
       </Col>
     )
   }
