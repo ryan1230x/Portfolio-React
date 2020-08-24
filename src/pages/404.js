3adc80d15370d61bc8986161e2e544b6b9afd117
ryan1230x <ryan1230tenerife@gmail.com>
2020-08-24 15:02:28 +0100

    added SVG, created layout for page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,31 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const styles = {
+  root:{
+    position:'absolute',
+    top:'50%',
+    textAlign:'center',
+    translateY:'-50%',
+    width:'100%'
+  },
+  img: {
+    height:'auto',
+    maxWidth:'100%'
+  }
+}
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className={styles.root}>
+      <h1>NOT FOUND</h1>
+      <img
+        src={require(../../404.svg)}
+        className={styles.img}
+        alt="404 svg"
+       />
+    </section>
   </Layout>
 )
 
